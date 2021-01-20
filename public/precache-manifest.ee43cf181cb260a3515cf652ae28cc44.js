e9b4bcea7e0c2f6a35f3c97a13bc1e5227d5e531
AnneVictoria <avduclaux@gmail.com>
2021-01-20 19:07:35 +0100

    deploy

---
+++ public/precache-manifest.ee43cf181cb260a3515cf652ae28cc44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21291211f2810e2aa5124736b8c355c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6b822883e74473567a",
+    "url": "/static/css/main.a006907f.chunk.css"
+  },
+  {
+    "revision": "5f3b42925cca9fe6c997",
+    "url": "/static/js/2.e0c5974c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e0c5974c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6b822883e74473567a",
+    "url": "/static/js/main.6214b590.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
