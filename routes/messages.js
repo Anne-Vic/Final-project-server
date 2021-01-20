922fad0505d5c17bd651532264ca100bd222dc6a
AnneVictoria <avduclaux@gmail.com>
2021-01-20 01:50:49 +0100

    update message route

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Message = require("../models/Message");
 
+// GET MESSAGES FOR ONE EVENT
 router.get("/by-event/:id", function (req, res, next) {
   console.log("SEARCH MESSAGE FOR EVENT ", req.params.id);
   Message.find({ event: req.params.id })
@@ -10,6 +11,7 @@ router.get("/by-event/:id", function (req, res, next) {
     .catch((err) => res.status(400).json(err));
 });
 
+// CREATE A MESSAGE FOR ONE EVENT
 router.post("/by-event/:id", function (req, res, next) {
   console.log("CREATE MESSAGE FOR EVENT ", req.params.id);
   console.log(req.body);
@@ -18,8 +20,22 @@ router.post("/by-event/:id", function (req, res, next) {
   updatedValues.event = req.params.id;
   Message.create(updatedValues)
     .then((message) => {
-      console.log("message created");
-      res.status(201).json(message);
+      message
+        .execPopulate("author")
+        .then((populatedMessage) => {
+          res.status(201).json(populatedMessage);
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
+
+//GET ALL MESSAGES OF ONE USER
+router.get("/mine", (req, res, next) => {
+  Message.find({ author: req.session.currentUser })
+    .populate("event author")
+    .then((eventDocuments) => {
+      res.status(200).json(eventDocuments);
     })
     .catch(next);
 });
